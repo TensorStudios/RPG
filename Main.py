71a2d97a87ed54cc6bc7bcdd6ec1900a447c664d
Devin Emnett <Emnettd@gmail.com>
2018-07-06 18:57:01 -0700

    Added Game Over Screen on death

--- Main.py
+++ Main.py
@@ -31,6 +31,7 @@ class Game:
         game_folder = path.dirname(__file__)
         img_folder = path.join(game_folder, 'img')
         self.map = Map(path.join(game_folder, 'SBMap.txt'))
+        self.gameover_font = path.join(img_folder, 'Game Over Font.TTF')
 
     def new(self):
         # initialize all variables and do all the setup for a new game
@@ -101,8 +102,48 @@ class Game:
     def show_start_screen(self):
         pass
 
+    def draw_text(self, text, font_name, size, color, x, y, align="nw"):
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        if align == "nw":
+            text_rect.topleft = (x, y)
+        if align == "ne":
+            text_rect.topright = (x, y)
+        if align == "sw":
+            text_rect.bottomleft = (x, y)
+        if align == "se":
+            text_rect.bottomright = (x, y)
+        if align == "n":
+            text_rect.midtop = (x, y)
+        if align == "s":
+            text_rect.midbottom = (x, y)
+        if align == "e":
+            text_rect.midright = (x, y)
+        if align == "w":
+            text_rect.midleft = (x, y)
+        if align == "center":
+            text_rect.center = (x, y)
+        self.screen.blit(text_surface, text_rect)
+
     def show_go_screen(self):
-        pass
+        self.screen.fill(BLACK)
+        self.draw_text("YOU DIED", self.gameover_font, 100, WHITE, WIDTH/2, HEIGHT/2, align='center')
+        self.draw_text("Press Any Key", self.gameover_font, 75, WHITE, WIDTH/2, HEIGHT*3/4, align='center')
+        pg.display.flip()
+        self.wait_for_key()
+
+    def wait_for_key(self):
+        pg.event.wait()
+        waiting = True
+        while waiting:
+            self.clock.tick(FPS)
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    waiting = False
+                    self.quit()
+                if event.type == pg.KEYUP:
+                    wating = False
 
 
 

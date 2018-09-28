15e9b233ba23641e42622337ffdca36d7a39166d
Devin Emnett <Emnettd@gmail.com>
2018-09-27 19:58:05 -0700

    Mob attack radius

--- Sprites.py
+++ Sprites.py
@@ -160,24 +160,27 @@ class Mob(pg.sprite.Sprite):
         self.rect.center = self.pos
         self.rot = 0
         self.health = MOB_HEALTH
+        self.target = game.player
 
     def update(self):
         if self.health <= 0:
             self.kill()
-        self.rot = (self.game.player.pos - self.pos).angle_to(vec(1, 0))
-        self.image = pg.Surface((TILESIZE, TILESIZE))
-        self.image.fill(BLACK)
-        self.rect = self.image.get_rect()
-        self.rect.center = self.pos
-        self.acc = vec(MOB_SPEED, 0).rotate(-self.rot)
-        self.acc += self.vel * -1
-        self.vel += self.acc * self.game.dt
-        self.pos += self.vel * self.game.dt + 0.5 * self.acc * self.game.dt ** 2
-        self.hit_rect.centerx = self.pos.x
-        collide_with_walls(self, self.game.walls, 'x')
-        self.hit_rect.centery = self.pos.y
-        collide_with_walls(self, self.game.walls, 'y')
-        self.rect.center = self.hit_rect.center
+        target_dist = self.target.pos - self.pos
+        if target_dist.length_squared() < DETECT_RADIUS**2:
+            self.rot = target_dist.angle_to(vec(1, 0))
+            self.image = pg.Surface((TILESIZE, TILESIZE))
+            self.image.fill(BLACK)
+            self.rect = self.image.get_rect()
+            self.rect.center = self.pos
+            self.acc = vec(MOB_SPEED, 0).rotate(-self.rot)
+            self.acc += self.vel * -1
+            self.vel += self.acc * self.game.dt
+            self.pos += self.vel * self.game.dt + 0.5 * self.acc * self.game.dt ** 2
+            self.hit_rect.centerx = self.pos.x
+            collide_with_walls(self, self.game.walls, 'x')
+            self.hit_rect.centery = self.pos.y
+            collide_with_walls(self, self.game.walls, 'y')
+            self.rect.center = self.hit_rect.center
 
 
 class Obstacle(pg.sprite.Sprite):
